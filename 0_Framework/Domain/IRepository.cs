4ef8a4c3a2460c56acbd9d1550a5ba6789986c5d
98913 <98913@LAPTOP-JQR66TAF>
2025-05-15 10:40:14 +0330

    Create IRrepository, RepositoryBase

---
+++ 0_Framework/Domain/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace _0_Framework.Domain;
+
+public interface IRepository<TKey,T> where T : class
+{
+ T? Get(TKey key);
+ List<T> Get();
+ void Create(T  entity);
+ void SaveChanges();
+
+}
+
---
+++ 0_Framework/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _0_Framework.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace _0_Framework.Infrastructure
+{
+    public class RepositoryBase<TKey,T>:IRepository<TKey,T> where T : class
+    {
+
+        private readonly DbContext _context;
+
+        public RepositoryBase(DbContext context)
+        {
+            _context = context;
+        }
+
+        public T? Get(TKey key) => _context.Find<T>(key);
+
+        public List<T> Get() => _context.Set<T>().ToList();
+
+        public void Create(T entity) => _context.Add(entity);
+
+        public void SaveChanges() => _context.SaveChanges();
+
+    }
+}
